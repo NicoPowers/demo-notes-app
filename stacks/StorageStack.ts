91ba7df4ebc70c549088ae7a31f9f6a267fec839
Nicolas Montoya <nicolasmontoya@yahoo.com>
2023-08-08 14:45:42 -0400

    latest, API isnt showing notes properly

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,20 @@
+import { Table, Bucket } from "sst/constructs";
+import { StackContext } from "sst/constructs";
+
+export function StorageStack({stack, app} : StackContext) {
+    // Create the DynamoDB table
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: "string",
+            noteId: "string"
+        },
+        primaryIndex: {
+            partitionKey: "userId", sortKey: "noteId"
+        }    
+    })
+
+    // Create S3 Bucket
+    const bucket = new Bucket(stack, "Uploads");
+
+    return {table, bucket}
+}
