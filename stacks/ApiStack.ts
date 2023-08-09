fba92263c196f2b47f7e5421a71b4169186e8c7e
EC2 Default User <ec2-user@ip-172-31-38-190.ec2.internal>
2023-08-09 01:59:19 +0000

    added API

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -16,7 +16,8 @@ export function ApiStack({stack, app} : StackContext) {
             "GET /notes": "packages/functions/src/list.main",
             "POST /notes": "packages/functions/src/create.main",
             "GET /notes/{id}": "packages/functions/src/get.main",
-            
+            "PUT /notes/{id}": "packages/functions/src/update.main",
+            "DELETE /notes/{id}": "packages/functions/src/delete.main"
         }
     });
 
