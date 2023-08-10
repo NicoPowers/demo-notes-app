722318f37e6de6b701101a606891f01d388b0192
Nicolas Montoya <nicolasmontoya@yahoo.com>
2023-08-10 08:35:53 -0500

    enabling CORS

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -15,7 +15,16 @@ export function StorageStack({ stack, app }: StackContext) {
   });
 
   // Create S3 Bucket
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "HEAD"],
+      },
+    ],
+  });
 
   return { table, bucket };
 }
