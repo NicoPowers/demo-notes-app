9b328bc1bbd9c8012554fc19fec150854777ff68
Nicolas Montoya <nicolasmontoya@yahoo.com>
2023-08-13 16:07:40 -0500

    got rid of test

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Scratch", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Scratch/i);
-  expect(linkElement).toBeInTheDocument();
-});
